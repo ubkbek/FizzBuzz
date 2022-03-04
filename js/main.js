8607b6e7d520cd59b2835d7984dd5a137594804d
ubkbek <ulugbekjovliyev01001@gmail.com>
2022-03-04 16:19:28 +0500

    add new algoritm to fizzbuzz function

--- js/main.js
+++ js/main.js
@@ -11,10 +11,25 @@ elForm.addEventListener("submit", function(event){
 
 // function fizzbuz
 function fizzbuzz(num){
-    if ( !(num % 3) || !(num % 5)){
-        if( num % 3 == 0 && num % 5 == 0 ) return "FIZZBUZZ"
-        else if (num % 3 == 0) return "FIZZ"
-        else return "BUZZ"
-    }
-    else return "Iltimos keyingi safar uchga yoki beshga karrali son kiriting :)";
+    // if(typeof num !== 'number')
+    //     return 'iltimos son kiriting';
+
+    if((num % 3 === 0) && (num % 5 === 0))
+        return 'FizzBuzz';
+
+    if(num % 3 == 0)
+        return 'Fizz';
+
+    if(num % 5 === 0)
+        return 'Buzz';
+
+    if(num % 3 && num % 5)
+        return num;
+
+    // if ( !(num % 3) || !(num % 5)){
+    //     if( num % 3 == 0 && num % 5 == 0 ) return "FIZZBUZZ"
+    //     else if (num % 3 == 0) return "FIZZ"
+    //     else return "BUZZ"
+    // }
+    // else return "Iltimos keyingi safar uchga yoki beshga karrali son kiriting :)";
 }
